d3fdfd61542ef30ecce060af9b73f9b960937040
notes-bin <17797274906@163.com>
2024-07-06 09:54:07 +0800

    sync.Mutex

---
+++ ch1/sync/mutex/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+type Counter struct {
+	mu    sync.Mutex
+	value int
+}
+
+func (c *Counter) Increment() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.value++
+}
+
+func (c *Counter) Value() int {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	return c.value
+}
+
+func main() {
+	var wg sync.WaitGroup
+	counter := Counter{}
+
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for j := 0; j < 1000; j++ {
+				counter.Increment()
+			}
+		}()
+	}
+
+	wg.Wait()
+	fmt.Printf("Final Counter Value: %d\n", counter.Value())
+}
